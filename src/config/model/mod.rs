30f76e4671cc7b5c46c09dc327767e96ed67d337
Dustin Pilgrim <dustin.pilgrim1997@gmail.com>
2025-12-10 03:16:18 -0400

    chore: renamed config/model/actions.rs to config/model/action.rs

--- src/config/model/mod.rs
+++ src/config/model/mod.rs
@@ -1,10 +1,10 @@
-pub mod actions;
+pub mod action;
 pub mod app_inhibit;
 pub mod lid;
 pub mod profile;
 pub mod stasis;
 
-pub use actions::*;
+pub use action::*;
 pub use app_inhibit::*;
 pub use lid::*;
 pub use profile::*;
